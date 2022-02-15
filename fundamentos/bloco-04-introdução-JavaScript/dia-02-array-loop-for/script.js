197edb274304a91cb11863b929e2a904b67948aa
Kaio G <kaiogreco91@gmail.com>
2022-02-15 19:50:27 -0300

    fixando for e for of

--- fundamentos/bloco-04-introdução-JavaScript/dia-02-array-loop-for/script.js
+++ fundamentos/bloco-04-introdução-JavaScript/dia-02-array-loop-for/script.js
@@ -19,3 +19,19 @@ let menu3 = ['Home', 'Serviços', 'Portfólio', 'Links'];
 menu3.push('Contato')
 
 console.log(menu3);
+
+// Utilize o for para imprimir os elementos da lista groceryList com o console.log() 
+
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for (let index = 0; index < groceryList.length; index += 1) {
+  console.log(groceryList[index]);
+}
+
+// Utilize o for/of para imprimir os elementos da lista names com o console.log() :
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for (let name of names) {
+  console.log(name);
+}
