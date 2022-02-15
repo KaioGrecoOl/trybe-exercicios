0f4f1e8f302533caf8b52d6d6cc38c89c1051831
Kaio G <kaiogreco91@gmail.com>
2022-02-15 17:40:17 -0300

    finaliza exercicios

--- fundamentos/bloco-04-introdução-JavaScript/script2.js
+++ fundamentos/bloco-04-introdução-JavaScript/script2.js
@@ -151,3 +151,69 @@ if ((first % 2 !== 0 || second % 2 !== 0 || third % 2 !== 0)) {
   isOdd = true;
 };
 console.log(isOdd);
+
+// Escreva um programa que se inicie com dois valores em duas constantes diferentes: o custo de um produto e seu valor de venda. A partir dos valores, calcule quanto de lucro (valor de venda descontado o custo do produto) a empresa terá ao vender mil desses produtos.
+// Atente que, sobre o custo do produto, incide um imposto de 20%
+//Seu programa também deve emitir uma mensagem de erro e encerrar caso algum dos seus valores de entrada seja menor que zero
+//O lucro de um produto é o resultado da subtração do valor de venda pelo custo do mesmo, sendo que o imposto de 20% também faz parte do valor de custo.
+
+const productCost = 50;
+const salesPrice =  100;
+
+// let totalProductCostInterest = productCost * 0.2;
+// let profit = (salesPrice - totalProductCostInterest) * 1000;
+// console.log(profit);
+
+if (productCost >= 0 && salesPrice >= 0) {
+  let totalProductCostInterest = productCost * 0.2
+  let profit = (salesPrice - totalProductCostInterest) * 1000;
+  console.log(profit);
+} else {
+  console.log('Error!');
+}
+
+// Uma pessoa que trabalha de carteira assinada no Brasil tem descontados de seu salário bruto o INSS e o IR. Faça um programa que, dado um salário bruto, calcule o líquido a ser recebido
+// A notação para um salário de R$1500,10, por exemplo, deve ser 1500.10. Para as faixas de impostos, use as seguintes referências
+// INSS (Instituto Nacional do Seguro Social)
+// Salário bruto até R$ 1.556,94: alíquota de 8%
+// Salário bruto de R$ 1.556,95 a R$ 2.594,92: alíquota de 9%
+// Salário bruto de R$ 2.594,93 a R$ 5.189,82: alíquota de 11%
+// Salário bruto acima de R$ 5.189,82: alíquota máxima de R$ 570,88
+// IR (Imposto de Renda)
+// Até R$ 1.903,98: isento de imposto de renda
+// De R$ 1.903,99 a 2.826,65: alíquota de 7,5% e parcela de R$ 142,80 a deduzir do imposto
+// De R$ 2.826,66 a R$ 3.751,05: alíquota de 15% e parcela de R$ 354,80 a deduzir do imposto
+// De R$ 3.751,06 a R$ 4.664,68: alíquota de 22,5% e parcela de R$ 636,13 a deduzir do imposto
+// Acima de R$ 4.664,68: alíquota de 27,5% e parcela de R$ 869,36 a deduzir do imposto.
+
+let salary = 2500.00;
+let inss;
+let ir;
+
+if (salary <= 1556.94) {
+  inss = salary * 0.08;
+} else if (salary <= 2594.92) {
+  inss = salary * 0.09;
+} else if (salary <= 5189.82) {
+  inss = salary * 0.11;
+} else {
+  inss = 570.88;
+};
+
+let baseSalary = salary - inss;
+
+if (baseSalary <= 1903.98) {
+  ir = 0;
+} else if (baseSalary <= 2826.65) {
+  ir = (baseSalary * 0.075) - 142.80;
+} else if (baseSalary <= 3751.05) {
+  ir = (baseSalary * 0.15) - 354.80;
+} else if (baseSalary <= 46664.68) {
+  ir = (baseSalary * 0.225) - 636.13
+} else {secondNumber
+  ir = (baseSalary * 0.275) - 869.36
+}
+console.log('Resultado: ' + (baseSalary - ir));
+
+
+
