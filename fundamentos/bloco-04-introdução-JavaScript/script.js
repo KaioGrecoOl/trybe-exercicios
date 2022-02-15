8e62a86b22e2cb61277636975190a3876c39d5eb
Kaio G <kaiogreco91@gmail.com>
2022-02-14 22:41:51 -0300

    condições if else

--- fundamentos/bloco-04-introdução-JavaScript/script.js
+++ fundamentos/bloco-04-introdução-JavaScript/script.js
@@ -39,4 +39,16 @@ console.log(area);
 const perimeter = (base * 2) + (height * 2);
 console.log(perimeter);
 
+// Crie uma constante que receba a nota de uma pessoa candidata em um desafio técnico, e atribua a ela um valor entre 1 e 100;
 
+const candidateGrade = 78;
+
+// Implemente uma lógica que verifique se a pessoa candidata foi aprovada, reprovada ou se ela está na lista de espera. Para isso, considere as seguintes informações: Se a nota for maior ou igual a 80, imprima "Parabéns, você foi aprovada(o)!", Se a nota for menor que 80 e maior ou igual a 60, imprima "Você está na nossa lista de espera",Se a nota for menor que 60, imprima "Você foi reprovada(o)" 
+
+if (candidateGrade >= 80) {
+  console.log('Parabéns, você foi aprovada(o)!');
+} else if (candidateGrade < 80 && candidateGrade >= 60) {
+  console.log('Você está na nossa lista de espera');
+} else if (candidateGrade < 60) {
+  console.log('Você foi reprovada(o)');
+}
