08b582023ef7170a9df889733b9aa88735598eac
Kaio G <kaiogreco91@gmail.com>
2022-03-18 18:36:49 -0300

    utilizando o componentDidUpdate, shouldComponentUpdate e WillUnmount

--- desenvolvimento-front-end/bloco-12-cliclo-de-vida-componentes/timer/timer/src/components/Timer.jsx
+++ desenvolvimento-front-end/bloco-12-cliclo-de-vida-componentes/timer/timer/src/components/Timer.jsx
@@ -6,17 +6,43 @@ class Timer extends React.Component {
     this.state = {
       seconds: 0,
       phases: ['🫁 Inspire...', '😑 Segure...', '😮‍💨 Expire...'],
-      phaseIndex: 2
+      phaseIndex: 0
     }
   }
 
+  // inicializa a contagem
   componentDidMount() {
     const ONE_SECOND = 1000;
-    setInterval(() => {
+    this.intervalId = setInterval(() => {
       this.setState((prevState) =>({
         seconds: prevState.seconds + 1
       }))
     }, ONE_SECOND)
+    console.log(this.intervalId);
+  }
+
+// manipula a contagem
+  componentDidUpdate(_prevProps, prevState) {
+    const TIME_LIMIT_IN_SECONDS = 5;
+    const TWO = 2;
+    const ZERO = 0;
+    if (prevState.seconds === TIME_LIMIT_IN_SECONDS){
+      this.setState({ seconds: 0,
+      phaseIndex: prevState.phaseIndex === TWO ? ZERO : prevState.phaseIndex + 1
+      })
+    }
+    // console.log(prevState.seconds);
+  }
+
+  shouldComponentUpdate(nextProps, nextState){
+    console.log(nextState.seconds);
+    console.log(nextState.phaseIndex);
+    return true;
+  }
+  
+  componentWillUnmount() {
+    console.log('vou desmontar');
+    clearInterval(this.intervalId)
   }
 
   render() {
