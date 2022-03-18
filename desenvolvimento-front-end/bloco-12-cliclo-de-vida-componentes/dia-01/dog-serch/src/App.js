06af2a0d777d2153fcd07080cf2fe3cf8c58121a
Kaio G <kaiogreco91@gmail.com>
2022-03-18 15:24:32 -0300

    finalizando bloco

--- desenvolvimento-front-end/bloco-12-cliclo-de-vida-componentes/dia-01/dog-serch/src/App.js
+++ desenvolvimento-front-end/bloco-12-cliclo-de-vida-componentes/dia-01/dog-serch/src/App.js
@@ -4,10 +4,10 @@ import React, { Component } from 'react';
 
 class App extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
-      base: '',
-    }
+      base: "",
+    };
     // this.fetchDog = this.fetchDog.bind(this);
   }
   componentDidMount() {
@@ -16,22 +16,21 @@ class App extends Component {
 
   fetchDog = () => {
     fetch("https://dog.ceo/api/breeds/image/random")
-    .then(response => response.json())
-    .then(data => {
-      this.setState({base: data})
-    })
-  }
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({ base: data });
+      });
+  };
   render() {
     if (this.state.base === "") return "Loading...";
     return (
       <main>
-        <img src={ this.state.base.message } alt='imgdog'></img>
+        <img src={this.state.base.message} alt="imgdog"></img>
         <p>cachorros!</p>
         <button onClick={this.fetchDog}>Oi</button>
       </main>
-    )
+    );
   }
-
 }
 
 export default App;
