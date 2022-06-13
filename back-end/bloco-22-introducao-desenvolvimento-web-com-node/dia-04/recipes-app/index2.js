26d24b05afd7f8b9ab955774f0809545a33686c1
Kaio G <kaiogreco91@gmail.com>
2022-06-13 15:19:38 -0300

    criando mensagem de erro para rota inexistente

--- back-end/bloco-22-introducao-desenvolvimento-web-com-node/dia-04/recipes-app/index2.js
+++ back-end/bloco-22-introducao-desenvolvimento-web-com-node/dia-04/recipes-app/index2.js
@@ -48,6 +48,10 @@ app.put('/recipes/:id', function (req, res) {
   res.status(204).end();
 });
 
+app.all('*', function (req, res) {
+	return res.status(404).json({ message: `Rota '${req.path}' não existe!`});
+});
+
 app.listen(3001, () => {
   console.log('Aplicação ouvindo na porta 3001');
 });
