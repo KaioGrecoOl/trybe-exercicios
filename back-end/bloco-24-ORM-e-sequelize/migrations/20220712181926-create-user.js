3b3965466080a8f0c44929c50e3ef05432821918
Kaio G <kaiogreco91@gmail.com>
2022-07-12 16:16:15 -0300

    feat(migrations)realiza as alterações no arquivo que será responsável por criar a tabela

--- back-end/bloco-24-ORM-e-sequelize/migrations/20220712181926-create-user.js
+++ back-end/bloco-24-ORM-e-sequelize/migrations/20220712181926-create-user.js
@@ -1,7 +1,12 @@
 'use strict';
 module.exports = {
+  //JSdocs
+  /**
+   * @param {import('sequelize').queryInterface } queryInterface 
+   * @param {import('sequelize').Sequelize} Sequelize 
+   */
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Users', {
+    await queryInterface.createTable('users', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -11,17 +16,32 @@ module.exports = {
       email: {
         type: Sequelize.STRING
       },
+      passwordHash: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        field: 'password_hash'
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false, 
+      },
+      phone: {
+        type: Sequelize.STRING,
+        allowNull: false, 
+      },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+        field: 'created_at'
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+        field: 'updated_at'
       }
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Users');
+    await queryInterface.dropTable('users');
   }
 };
