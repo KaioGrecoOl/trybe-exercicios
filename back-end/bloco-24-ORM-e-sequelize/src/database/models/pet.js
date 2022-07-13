c350e095160f319cc103c0a3df28ef59e81d90a5
Kaio G <kaiogreco91@gmail.com>
2022-07-13 16:19:46 -0300

    feat(models):cria o arquivo models pet

--- back-end/bloco-24-ORM-e-sequelize/src/database/models/pet.js
+++ back-end/bloco-24-ORM-e-sequelize/src/database/models/pet.js
@@ -1,23 +1,18 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class Pet extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
-  Pet.init({
-    name: DataTypes.STRING
+const createPetModel = (sequelize, DataTypes) => {
+  const Pet = sequelize.define('Pet', {
+    name: DataTypes.STRING,
+    userId: DataTypes.INTEGER
   }, {
-    sequelize,
-    modelName: 'Pet',
+    underscored: true,
+    tableName: 'pets',
   });
-  return Pet;
+
+  Pet.associate = (models) => {
+    Pet.belongsTo(models.User, {as: 'user', foreignKey: 'userId'})
+  }
+
+  return Pet
 };
+
+module.exports = createPetModel;
