7242df91d5b168a4b26ee9b2b1cd119729808f25
Kaio G <kaiogreco91@gmail.com>
2022-07-13 17:18:45 -0300

    feat(model):cria funcionalidades do arquivo models vet

--- back-end/bloco-24-ORM-e-sequelize/src/database/models/vet.js
+++ back-end/bloco-24-ORM-e-sequelize/src/database/models/vet.js
@@ -1,23 +1,12 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class Vet extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
-  Vet.init({
-    name: DataTypes.STRING
+const createVetModel = (sequelize, DataTypes) => {
+  const Vet = sequelize.define('Vet', {
+    name: DataTypes.STRING,
   }, {
-    sequelize,
-    modelName: 'Vet',
+    underscored: true,
+    tableName: 'vets',
   });
+
   return Vet;
 };
+
+module.exports = createVetModel;
