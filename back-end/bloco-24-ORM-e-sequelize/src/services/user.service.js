587befbccfcd20a2e505e21e860be8b588a4534c
Kaio G <kaiogreco91@gmail.com>
2022-07-14 16:08:57 -0300

    feat(encriptando senha)cria funcionalidade para validar o corpo da requisição(joi) bem como instala e faz uso do bcrypt

--- back-end/bloco-24-ORM-e-sequelize/src/services/user.service.js
+++ back-end/bloco-24-ORM-e-sequelize/src/services/user.service.js
@@ -2,12 +2,38 @@ const {User, Pet} = require('../database/models/index')
 const Sequelize = require('sequelize');
 const config = require('../database/config/config');
 const sequelize = new Sequelize(config.development);
+const Joi = require('joi');
+const passwordHashing = require('../services/password.services');
 
+const validateBody = (data) => {
+  const schema = Joi.object({
+  email: Joi.string().email().required(),
+  name: Joi.string().required(),
+  password: Joi.string().required(),
+  confirmPassword: Joi.string().required(),
+  phone: Joi.string().required(),
+})
+
+const { error, value } = schema.validate(data);
+
+if (error) throw error;
+
+return value;
+}
 
 const getAllUsers = async () => {
   const result = await User.findAll();
   return result;
 };
+
+const createUser = async ({email, password, confirmPassword, name, phone}) => {
+  const validate = validateBody({email, password, confirmPassword, name, phone})
+  const passwordEncrypted = passwordHashing.encryptPassword(password)
+  const result = await User.create(
+      {email, password_hash: passwordEncrypted, name, phone}
+  )
+  return result;
+}
 
 const createUserComPet = async ({email, password_hash, name, phone, namepet }) => {
   const t = await sequelize.transaction();
@@ -34,4 +60,4 @@ const createUserComPet = async ({email, password_hash, name, phone, namepet }) =
   }
 }
 
-module.exports = {getAllUsers, createUserComPet}
+module.exports = {getAllUsers, createUserComPet, createUser}
