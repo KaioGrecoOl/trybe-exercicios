f3ac60ec6ba1011e0fe06ece45346504ffd6717a
Kaio G <kaiogreco91@gmail.com>
2022-07-17 18:28:03 -0300

    feat(token)adiciona funcionalidade para gerar token

--- back-end/bloco-24-ORM-e-sequelize/src/app.js
+++ back-end/bloco-24-ORM-e-sequelize/src/app.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const cors = require('cors'); 
 const usersRouter = require('./routes/user.routes');
-const petsRouter = require('./routes/pet.routes')
+const petsRouter = require('./routes/pet.routes');
+const authRouter = require('./routes/auth.routes');
+const authController = require('../src/controllers/auth.controller');
+const errorHandler = require('../src/middleware/error.middleware');
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+require('express-async-errors');
 
 app.use('/users', usersRouter);
+app.use('/signin', authRouter);
+app.use(authController.validateToken)
 app.use('/pets', petsRouter);
+app.use(errorHandler)
 
 module.exports = app;
--- back-end/bloco-24-ORM-e-sequelize/src/services/user.service.js
+++ back-end/bloco-24-ORM-e-sequelize/src/services/user.service.js
@@ -3,61 +3,61 @@ const Sequelize = require('sequelize');
 const config = require('../database/config/config');
 const sequelize = new Sequelize(config.development);
 const Joi = require('joi');
-const passwordHashing = require('../services/password.services');
+const passwordHashing = require('../services/password.service')
 
 const validateBody = (data) => {
-  const schema = Joi.object({
-  email: Joi.string().email().required(),
-  name: Joi.string().required(),
-  password: Joi.string().required(),
-  confirmPassword: Joi.string().required(),
-  phone: Joi.string().required(),
-})
+    const schema = Joi.object({
+    email: Joi.string().email().required(),
+    name: Joi.string().required(),
+    password: Joi.string().required(),
+    confirmPassword: Joi.string().required(),
+    phone: Joi.string().required(),
+  })
 
-const { error, value } = schema.validate(data);
+  const { error, value } = schema.validate(data);
 
-if (error) throw error;
+  if (error) throw error;
 
-return value;
+  return value;
 }
 
 const getAllUsers = async () => {
-  const result = await User.findAll();
-  return result;
-};
+    const result = await User.findAll()
+    return result
+}
 
 const createUser = async ({email, password, confirmPassword, name, phone}) => {
-  const validate = validateBody({email, password, confirmPassword, name, phone})
-  const passwordEncrypted = passwordHashing.encryptPassword(password)
-  const result = await User.create(
-      {email, password_hash: passwordEncrypted, name, phone}
-  )
-  return result;
+    const validate = validateBody({email, password, confirmPassword, name, phone})
+    const passwordEncrypted = passwordHashing.encryptPassword(password)
+    const result = await User.create(
+        {email, password_hash: passwordEncrypted, name, phone}
+    )
+    return result
 }
 
 const createUserComPet = async ({email, password_hash, name, phone, namepet }) => {
-  const t = await sequelize.transaction();
+    const t = await sequelize.transaction();
 
-  try {
+    try {
 
-      const user = await User.create(
-      { email, name, phone, password_hash },
-      { transaction: t },
-      );
+        const user = await User.create(
+        { email, name, phone, password_hash },
+        { transaction: t },
+        );
 
-      await Pet.create(
-      { userId: user.id, name: namepet  },
-      { transaction: t },
-      );
+        await Pet.create(
+        { userId: user.id, name: namepet  },
+        { transaction: t },
+        );
 
-      await t.commit();
+        await t.commit();
 
-      return true
-  } catch (e) {
-      await t.rollback();
-      console.log(e.message);
-      return e
-  }
+        return true
+    } catch (e) {
+        await t.rollback();
+        console.log(e.message);
+        return e
+    }
 }
 
 module.exports = {getAllUsers, createUserComPet, createUser}
