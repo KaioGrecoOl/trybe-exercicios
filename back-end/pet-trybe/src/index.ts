e7d9c389b061bd193baba2a536f15a4920473e13
Kaio G <kaiogreco91@gmail.com>
2022-08-16 17:05:37 -0300

    feat: cria o arquivo index com as configurações iniciai
    Realiza a instalação dos pacotes necessários para o projeto, express, typescript, sequelize e node

---
+++ back-end/pet-trybe/src/index.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = 8000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.status(StatusCodes.OK).send('Express + Typescript');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http:/localhost:${PORT}`);
+  
+})
