3c0a19e04469f1927a4ae696abe48e7970611e1e
Kaio G <kaiogreco91@gmail.com>
2022-11-16 20:13:40 -0300

    feat: :sparkles: função usando o get para obter o nome dos usuários e a url com o for

--- ciencia-computação/exercicio-rapagem-dados/dia01/sample1.py
+++ ciencia-computação/exercicio-rapagem-dados/dia01/sample1.py
@@ -1,5 +1,13 @@
 import requests
 
+# Realizando requisição e exibindo o conteúdo
+# response = requests.get("https://httpbin.org/encoding/utf8")
+# print(response.text)
 
-response = requests.get("https://httpbin.org/encoding/utf8")
-print(response.text)
+
+# Realizando requisição e retornar o nome de usuário e url
+
+response = requests.get("https://api.github.com/users")
+users = response.json()
+for user in users:
+  print(f"{user['login']} {user['url']}")
