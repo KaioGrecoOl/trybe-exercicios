badb3e04feba2adf7e1482b3cc678a7bbe1ea211
Kaio G <kaiogreco91@gmail.com>
2022-11-16 22:02:11 -0300

    feat: :zap: função que realiza extração título, preço, descrição e url contendo a imagem de capa do livro

--- ciencia-computação/exercicio-rapagem-dados/dia01/sample1.py
+++ ciencia-computação/exercicio-rapagem-dados/dia01/sample1.py
@@ -1,4 +1,5 @@
 import requests
+from parsel import Selector
 
 # Realizando requisição e exibindo o conteúdo
 # response = requests.get("https://httpbin.org/encoding/utf8")
@@ -7,7 +8,26 @@ import requests
 
 # Realizando requisição e retornar o nome de usuário e url
 
-response = requests.get("https://api.github.com/users")
-users = response.json()
-for user in users:
-  print(f"{user['login']} {user['url']}")
+# response = requests.get("https://api.github.com/users")
+# users = response.json()
+# for user in users:
+#   print(f"{user['login']} {user['url']}")
+
+# Realizando requisição e extraindo título, preço, descrição e url contendo a imagem de capa do livro
+url = "http://books.toscrape.com/catalogue/"
+
+response = requests.get(url + "the-grand-design_405/index.html")
+selector = Selector(response.text)
+
+title = selector.css("h1::text").get()
+
+price = selector.css(".product_main > .price_color::text").re_first(r"\d*\.\d{2}")
+
+description = selector.css("#product_description ~ p::text").get()
+suffix = "...more"
+if description.endswith(suffix):
+    description = description[:-len(suffix)]
+
+cover = url + selector.css("#product_gallery img::attr(src)").get()
+
+print(title, price, description, cover, sep=",")
